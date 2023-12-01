e8d34ee30b0664e7e5eeca8a4cb56930850a01ac
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-01 21:04:07 +0330

    feat(sub route): Add '/auth' route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,10 @@ import { FastifyPluginCallback } from "fastify";
 import authRoutes from "./auth.routes";
 
 const plugin: FastifyPluginCallback = (fastify, options, done) => {
+  fastify.register(authRoutes, {
+    prefix: "/auth",
+  });
+  done();
 };
 
 export default plugin;
