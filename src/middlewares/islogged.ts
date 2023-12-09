268f6dedbe018b3d52d86080f655e9512fb56650
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-09 14:44:12 +0330

    feat: Add isLogged middleware to check if the user is logged or not

---
+++ src/middlewares/islogged.ts
@@ -0,0 +1,53 @@
+import { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from "fastify";
+import sendResponse from "../utils/sendResponse";
+import sendErrorResponse from "../utils/sendErrorResponse";
+import UserServices from "../services/user.services";
+import { JwtPayload } from "jsonwebtoken";
+
+const isLogged = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+  done: HookHandlerDoneFunction
+) => {
+  const userServices: UserServices = request.diScope.resolve("userServices");
+  const token = request.headers.token as string;
+
+  try {
+    if (!token) {
+      return sendResponse(reply, {
+        status: "error",
+        statusCode: 401,
+        message:
+          "Token is required. Please include the token in the 'token' header.",
+      });
+    }
+
+    const decodedToken: JwtPayload | string = userServices.decodeToken(token);
+    if (typeof decodedToken === "string") {
+      return sendResponse(reply, {
+        statusCode: 403,
+        status: "error",
+        message: "Invalid token fotrmat.",
+      });
+    }
+
+    if (request.url === "/auth/complete-profile") {
+      request.user = decodedToken;
+      return done();
+    }
+    if (decodedToken?.isProfileCompleted === false) {
+      return sendResponse(reply, {
+        statusCode: 403,
+        status: "error",
+        message:
+          "Your profile isn't completed. So navigate to '/auth/complete-profile' route by 'PUT' request to complete your profile info. ",
+      });
+    }
+    request.user = decodedToken;
+    return done();
+  } catch (error) {
+    return sendErrorResponse(reply, error);
+  }
+};
+
+export default isLogged;
