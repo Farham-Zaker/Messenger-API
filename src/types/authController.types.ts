be3c9223406194fac84fb763ecc0e63c3dab1da1
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-09 14:36:35 +0330

    feat(auth): Add types for '/verify-phone-number' controller

--- src/types/authController.types.ts
+++ src/types/authController.types.ts
@@ -5,3 +5,37 @@ export type SendVerificationCodeBodyType = {
   areaCode: string;
   phoneNumber: string;
 };
+export type VerifyPhoneNumberBodyType = {
+  areaCode: string;
+  phoneNumber: string;
+  verificationCode: string;
+};
+export type FindUserTypes = {
+  userId?: string;
+  firstName?: string;
+  lastName?: string;
+  username?: string;
+  phoneNumber?: string;
+  bio?: string;
+  email?: string;
+  password?: string;
+  createdAt?: Date;
+  areaCode?: AreaCodeTypes;
+};
+type AreaCodeTypes = {
+  areaCodeId?: string;
+  areaCode?: string;
+  area?: string;
+};
+export type CreateUserTypes = {
+  userId: string;
+  firstName: string | null;
+  lastName: string | null;
+  username: string | null;
+  phoneNumber: string;
+  areaCodeId: string;
+  bio: string | null;
+  email: string | null;
+  password: string | null;
+  createdAt: Date;
+};
