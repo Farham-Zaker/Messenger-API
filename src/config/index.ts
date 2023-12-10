0303c4b6e6d4d2c5d6458e82d19cf3a2b14fdcbc
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-10 09:53:28 +0330

    feat(config): Add host to config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 const configs: ConfigTypes = {
+  host: "localhost",
   port: 8000,
 };
 
 interface ConfigTypes {
+  host: string;
   port: number;
 }
 
-export default configs
+export default configs;
