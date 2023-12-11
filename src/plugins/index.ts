1cd042b144f52785e831cd17512021727cfd473f
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-11 23:24:31 +0330

    refactor: Update plugins root

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
 import diConfig from "./di.config";
-
+import swaggerConfig from "./swagger.config";
 const plugins= (server: FastifyInstance) =>{
     diConfig(server)
+    swaggerConfig(server)
 }
 
 export default plugins
