a530352e891983baeb03e4c3c47e3cdfee39a254
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-11 23:35:26 +0330

    feat(auth): Add docs for '/verify-phone-number' route

--- src/docs/auth.docs.ts
+++ src/docs/auth.docs.ts
@@ -34,6 +34,48 @@ const authDocs = {
       },
     },
   },
+  "/verify-phone-number": {
+    get: {
+      tags: ["Authentication"],
+      summary:
+        "Verify user phone number if the verification code that sent is valid.",
+      parameters: [
+        {
+          name: "body",
+          in: "body",
+          required: true,
+          schema: {
+            type: "object",
+            properties: {
+              areaCode: {
+                type: "string",
+              },
+              phoneNumber: {
+                type: "string",
+              },
+              verificationCode: {
+                type: "string",
+              },
+            },
+          },
+        },
+      ],
+      responses: {
+        200: {
+          description: "Success. Phone number was verified.",
+        },
+        401: {
+          description: "Error. Invalid verififcation code.",
+        },
+        404: {
+          description: "Error. There is not area code such code.",
+        },
+        500: {
+          description: "Internal Server Error",
+        },
+      },
+    },
+  },
 };
 
 export default authDocs;
