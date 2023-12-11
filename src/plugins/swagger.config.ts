e2dda0f5becf0a2f499ff0325d4e96330cb4c961
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-11 23:21:53 +0330

    feat(swagger): Config package

---
+++ src/plugins/swagger.config.ts
@@ -0,0 +1,32 @@
+import { FastifyInstance, FastifyPluginCallback } from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import configs from "../config";
+
+import authDocs from "../docs/auth.docs";
+
+const swaggerConfig = (server: FastifyInstance) => {
+  const swaggerOptions = {
+    swagger: {
+      info: {
+        title: "Real-Time Messaging API",
+        description:
+          "Welcome to our real-time messaging API built using Node.js, TypeScript, and Socket.IO. This project empowers developers to integrate robust messaging functionality into their applications. Leveraging the power of Socket.IO, our API enables seamless, bidirectional communication between clients and the server, facilitating instant message delivery and real-time updates.",
+        version: "1.0.0",
+      },
+      host: configs.host,
+      paths: {
+},
+    },
+    exposeRoute: false,
+  };
+  const swaggerUiOptions = {
+    routePrefix: "/docs",
+    exposeRoute: false,
+  };
+
+  server.register(fastifySwagger, swaggerOptions);
+  server.register(fastifySwaggerUi, swaggerUiOptions);
+};
+
+export default swaggerConfig;
