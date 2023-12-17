093d7b58266bc22aa211d829c42748c73efe160f
Farham-Zaker <farham.zaker007@gmail.com>
2023-12-17 18:54:28 +0330

    refactor: Update validator logic

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,27 +1,33 @@
 import { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from "fastify";
 import sendResponse from "../utils/sendResponse";
 
-const validate = (schema: any) => {
+type ParametersType = {
+  target: string;
+  schema: any;
+};
+const validate = ({ target, schema }: ParametersType) => {
   return (
     request: FastifyRequest,
     reply: FastifyReply,
     done: HookHandlerDoneFunction
   ) => {
-    const { error } = schema.validate(request.body);
-    if (error) {
-      const errors = error.details.map((detail: any) => {
-        return {
-          message: detail.message,
-          path: detail.path.join("."),
-        };
-      });
-      return sendResponse(reply, {
-        status: "error",
-        statusCode: 400,
-        errors,
-      });
-    } else {
-      done();
+    if (target === "body") {
+      const { error } = schema.validate(request.body);
+      if (error) {
+        const errors = error.details.map((detail: any) => {
+          return {
+            message: detail.message,
+            path: detail.path.join("."),
+          };
+        });
+        return sendResponse(reply, {
+          status: "error",
+          statusCode: 400,
+          in: "body",
+          errors,
+        });
+      }
+      return done();
     }
   };
 };
