e5b7444467ebc41e6adfc2998d4f92ee48aa4ab3
Farham-Zaker <farham.zaker007@gmail.com>
2024-01-13 11:22:25 +0330

    feat(group): Add '/create' route

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -1,6 +1,11 @@
 import { FastifyPluginCallback } from "fastify";
+import groupControllers from "../controllers/group.controllers";
 
 const groupRoutesPlugin: FastifyPluginCallback = (fastify, option, done) => {
+  fastify.route({
+    url: "/create",
+    method: "POST",
+  });
   done();
 };
 
