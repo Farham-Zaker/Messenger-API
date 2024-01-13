b4908d01b4d98537f82429330a700d99e5dc0dab
Farham-Zaker <farham.zaker007@gmail.com>
2024-01-13 10:54:50 +0330

    feat(sub routes): Config '/group' route

---
+++ src/routes/group.routes.ts
@@ -0,0 +1,7 @@
+import { FastifyPluginCallback } from "fastify";
+
+const groupRoutesPlugin: FastifyPluginCallback = (fastify, option, done) => {
+  done();
+};
+
+export default groupRoutesPlugin;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { FastifyPluginCallback } from "fastify";
 import authRoutes from "./auth.routes";
 import privateChatRoutes from "./privateChat.routes";
 import messageRoutesPlugin from "./message.routes";
+import groupRoutesPlugin from "./group.routes";
 
 const plugin: FastifyPluginCallback = (fastify, options, done) => {
   fastify.register(authRoutes, {
@@ -11,6 +12,9 @@ const plugin: FastifyPluginCallback = (fastify, options, done) => {
     prefix: "/privateChat",
   });
   fastify.register(messageRoutesPlugin, { prefix: "/message" });
+  fastify.register(groupRoutesPlugin, {
+    prefix: "/group",
+  });
   done();
 };
 
