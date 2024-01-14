b1a85061262c1ce0f0145ca7dfb0708e273e132c
Farham-Zaker <farham.zaker007@gmail.com>
2024-01-14 11:31:27 +0330

    feat(group): Add required sevices for '/add-member' route with its types

--- src/services/group.services.ts
+++ src/services/group.services.ts
@@ -2,14 +2,39 @@ import prismaServices from "../prisma/prismaServices";
 import {
   CreateGroupParametersTypes,
   GroupTypes,
+  AddMemberToGroupParamtersTyps,
+  AddedMemberTypes,
+  FindOneGroupMemberParametersTypes,
+  GroupMemberTypes,
 } from "../types/groupServices.types";
+import databaseSelector from "../utils/databaseSelector";
 
 class GroupServices {
   async create(data: CreateGroupParametersTypes): Promise<GroupTypes> {
     const group: GroupTypes = await prismaServices.groups.create({
-      data
+      data,
     });
     return group;
+  }
+  async addMember(
+    data: AddMemberToGroupParamtersTyps
+  ): Promise<AddedMemberTypes> {
+    const groupMember: AddedMemberTypes =
+      await prismaServices.groups_members.create({
+        data,
+      });
+    return groupMember;
+  }
+  async findOneGroupMember({
+    condition,
+    selectedFields,
+  }: FindOneGroupMemberParametersTypes): Promise<GroupMemberTypes | null> {
+    const groupMember: GroupMemberTypes | null =
+      await prismaServices.groups_members.findFirst({
+        where: condition,
+        select: databaseSelector("groups_members", selectedFields),
+      });
+    return groupMember;
   }
 }
 export default GroupServices;
--- src/types/groupServices.types.ts
+++ src/types/groupServices.types.ts
@@ -13,3 +13,44 @@ export type GroupTypes = {
   updatedAt: Date;
   createdAt: Date;
 };
+export type AddMemberToGroupParamtersTyps = {
+  userId: string;
+  groupId: string;
+};
+export type AddedMemberTypes = {
+  memberId: string;
+  groupId: string;
+  userId: string;
+};
+export type FindOneGroupMemberParametersTypes = {
+  condition: {
+    memberId?: string;
+    userId?: string;
+    groupId?: string;
+  };
+
+  selectedFields: {
+    groups_members: string[];
+    users?: string[];
+    group?: string[];
+  };
+};
+export type GroupMemberTypes = {
+  memberId?: string;
+  groupId?: string;
+  group?: GroupTypes;
+  userId?: string;
+  user?: UserTypes;
+};
+type UserTypes = {
+  userId: string;
+  firstName: string | null;
+  lastName: string | null;
+  username: string | null;
+  areaCodeId: string;
+  phoneNumber: string;
+  bio: string | null;
+  email: string | null;
+  password: string | null;
+  createdAt: Date;
+};
