0e511be281bbb9cd14a50fad2e86fa100ab66049
Farham-Zaker <farham.zaker007@gmail.com>
2024-01-24 19:34:08 +0330

    feat: Install and config @fastify/multipart and pump pckage

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
+import multipart from "@fastify/multipart";
 import diConfig from "./di.config";
 import swaggerConfig from "./swagger.config";
-const plugins= (server: FastifyInstance) =>{
-    diConfig(server)
-    swaggerConfig(server)
-}
+const plugins = (server: FastifyInstance) => {
+  diConfig(server);
+  swaggerConfig(server);
+  server.register(multipart);
+};
 
-export default plugins
+export default plugins;
