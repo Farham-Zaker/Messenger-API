9e44482433d4109ad04e4c0be24a1da6ee2afcaf
Farham-Zaker <farham.zaker007@gmail.com>
2024-01-25 02:42:59 +0330

    refactor: Update sendResponse util

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -3,6 +3,8 @@ import { FastifyReply } from "fastify";
 type ResponseTypes = {
   statusCode: number;
   status: string;
+  error?: string;
+  message?: string;
   [key: string]: any;
 };
 const sendResponse = (reply: FastifyReply, response: ResponseTypes) => {
