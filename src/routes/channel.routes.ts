ec97d22b3cccef81aa7fe0a0f4a8a89e40c6db7d
Farham-Zaker <farham.zaker007@gmail.com>
2024-01-29 19:11:35 +0330

    feat(channel): Add '/create' route

--- src/routes/channel.routes.ts
+++ src/routes/channel.routes.ts
@@ -1,10 +1,20 @@
 import { FastifyPluginCallback } from "fastify";
 import validate from "../middlewares/validation.middleware";
 import isLogged from "../middlewares/islogged";
+
 const channelRoutesPlugin: FastifyPluginCallback = async (
   fastify,
   options,
   done
-) => {};
+) => {
+  fastify.route({
+    url: "/create",
+    method: "POST",
+    preHandler: [
+      isLogged,
+    ],
+    handler: () => {},
+  });
+};
 
 export default channelRoutesPlugin;
