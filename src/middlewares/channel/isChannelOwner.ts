b3ab8489787e8985575492541ed6b3feae933440
Farham-Zaker <farham.zaker007@gmail.com>
2024-01-30 17:53:56 +0330

    feat(channel): Add middleware for checking if logged in user is owner of channel or not

---
+++ src/middlewares/channel/isChannelOwner.ts
@@ -0,0 +1,37 @@
+import { preHandlerHookHandler } from "fastify";
+import ChannelServices from "../../services/channel.services";
+import sendResponse from "../../utils/sendResponse";
+import sendErrorResponse from "../../utils/sendErrorResponse";
+
+const isChannelOwner: preHandlerHookHandler = async (request, reply, done) => {
+  const { channelId, userId } = request.body as {
+    channelId: string;
+    userId: string;
+  };
+  const user = request.user;
+  try {
+    const channelServices: ChannelServices =
+      request.diScope.resolve("channelServices");
+
+    const isChannelOwner: boolean = !!(await channelServices.findOneChannel({
+      condition: {
+        channelId,
+        ownerId: user?.userId,
+      },
+      selectedFields: {
+        channels: ["channelId"],
+      },
+    }));
+    if (!isChannelOwner) {
+      return sendResponse(reply, {
+        status: "error",
+        statusCode: 400,
+        message: "Just owner of group can add a user to admin of channel.",
+      });
+    }
+  } catch (error) {
+    return sendErrorResponse(reply, error);
+  }
+};
+
+export default isChannelOwner;
