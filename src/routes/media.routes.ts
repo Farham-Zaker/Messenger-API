c278b7a10782f10e455d2c142b5feb3fc10e523b
Farham-Zaker <farham.zaker007@gmail.com>
2024-02-15 19:20:51 +0330

    feat(media): Add '/send' route

--- src/routes/media.routes.ts
+++ src/routes/media.routes.ts
@@ -1,6 +1,16 @@
 import { FastifyPluginCallback } from "fastify";
+import isLogged from "../middlewares/islogged";
+import validate from "../middlewares/validation.middleware";
 
 const mediaRoutePlugin: FastifyPluginCallback = (fastify, option, done) => {
+  fastify.route({
+    url: "/send",
+    method: "POST",
+    preHandler: [
+      isLogged,
+    ],
+  });
+
   done();
 };
 
