5f23c97f1256de29f70b7c7174d516c58b39fc7a
Farham-Zaker <farham.zaker007@gmail.com>
2024-02-15 19:45:01 +0330

    feat: Add the posibility of multiple upload in upload util

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -29,7 +29,64 @@ const upload = async ({
   destination,
   uploadFileName,
 }: ParametersTypes): Promise<FunctionType | FunctionType[] | void> => {
-  
+  if (uploadType === "multiple") {
+    const uploadedFiles = [];
+    const files = request.files();
+    for await (let part of files) {
+      const uploadedFileName: string[] = part.filename.split(".");
+      const fileFormat: string = uploadedFileName[uploadedFileName.length - 1];
+
+      // Check if file format is invalid or not
+      const isFileFormatValid: boolean = !!acceptableFormats.find(
+        (format) => format === fileFormat
+      );
+      if (!isFileFormatValid) {
+        return sendResponse(reply, {
+          status: "error",
+          statusCode: 415,
+          message: `Unsupported Media Type. ${part.filename} format is invalid.`,
+        });
+      }
+
+      // Create folder if upload destination is not exist
+      if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination);
+      }
+      const uploadPath: string = path.join(
+        destination,
+        category || "",
+        uploadFileName,
+        `${uuid()}.${fileFormat}`
+      );
+
+      // Create folder if a folder with category name did not exist
+      const uploadPathWithCategoryName = path.join(destination, category || "");
+      if (!fs.existsSync(uploadPathWithCategoryName)) {
+        fs.mkdirSync(uploadPathWithCategoryName);
+      }
+
+      // Create folder if a folder did not exist for each private chat or channel or group
+      const uploadPathWithCategoryNameAndID = path.join(
+        destination,
+        category || "",
+        uploadFileName
+      );
+      if (!fs.existsSync(uploadPathWithCategoryNameAndID)) {
+        fs.mkdirSync(uploadPathWithCategoryNameAndID);
+      }
+      
+      // Upload
+      await pump(part.file, fs.createWriteStream(uploadPath));
+
+      uploadedFiles.push({
+        status: "success",
+        fileName: `${uuid()}.${fileFormat}`,
+        filePath: path.join(__dirname, "../../", uploadPath),
+        fileType: fileFormat,
+      });
+    }
+    return uploadedFiles;
+  } else if (uploadType === "single") {
     const file = await request.file();
 
     // Check file uploaded or not.
@@ -69,7 +126,7 @@ const upload = async ({
       fileName: `${uploadFileName}.${fileFromat}`,
       filePath: path.join(__dirname, "../../", uploadPath),
     };
-
+  }
 };
 
 export default upload;
