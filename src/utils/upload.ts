51ff6da1e74c33931bf7c77e01c054a9f3bfec20
Farham-Zaker <farham.zaker007@gmail.com>
2024-02-15 19:42:32 +0330

    refactor: Improve structure of upload util

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -3,62 +3,73 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import path from "path";
 import pump from "pump";
 import fs from "fs";
+import { v4 as uuid } from "uuid";
 type ParametersTypes = {
   request: FastifyRequest;
   reply: FastifyReply;
   acceptableFormats: string[];
-  uploadDestination: string;
-  desiredName: string;
+  uploadType: "single" | "multiple";
+  category?: string;
+  destination: string;
+  uploadFileName: string;
 };
+
 type FunctionType = {
-  uploadStatus: string;
+  status: string;
   filePath: string;
+  fileName: string;
 };
+
 const upload = async ({
   request,
   reply,
+  uploadType,
+  category,
   acceptableFormats,
-  uploadDestination,
-  desiredName,
-}: ParametersTypes): Promise<FunctionType | void> => {
-  const file = await request.file();
+  destination,
+  uploadFileName,
+}: ParametersTypes): Promise<FunctionType | FunctionType[] | void> => {
+  
+    const file = await request.file();
 
-  //   Check file uploaded or not.
-  if (!file) {
-    return sendResponse(reply, {
-      status: "error",
-      statusCode: 400,
-      message: "No file uplouded.",
-    });
-  }
+    // Check file uploaded or not.
+    if (!file) {
+      return sendResponse(reply, {
+        status: "error",
+        statusCode: 400,
+        message: "No file uplouded.",
+      });
+    }
 
-  //   Check the format of file.
-  const fileNmae: string[] = file.filename.split(".");
-  const fileFromat = fileNmae[fileNmae.length - 1];
-  const isFileFormatInvalid: boolean = !!acceptableFormats.find(
-    (format) => format === fileFromat
-  );
-  if (!isFileFormatInvalid) {
-    return sendResponse(reply, {
-      status: "error",
-      statusCode: 415,
-      message: "Unsupported Media Type. The file format is invalid.",
-    });
-  }
+    // Check the format of file.
+    const fileName: string[] = file.filename.split(".");
+    const fileFromat: string = fileName[fileName.length - 1];
+    const isFileFormatInvalid: boolean = !!acceptableFormats.find(
+      (format) => format === fileFromat
+    );
+    if (!isFileFormatInvalid) {
+      return sendResponse(reply, {
+        status: "error",
+        statusCode: 415,
+        message: "Unsupported Media Type. The file format is invalid.",
+      });
+    }
 
-  if (!fs.existsSync(uploadDestination)) {
-    fs.mkdirSync(uploadDestination);
-  }
-  const uploadPath = path.join(
-    uploadDestination,
-    `${desiredName}.${fileFromat}`
-  );
+    if (!fs.existsSync(destination)) {
+      fs.mkdirSync(destination);
+    }
+    const uploadPath = path.join(
+      destination,
+      `${uploadFileName}.${fileFromat}`
+    );
 
-  await pump(file.file, fs.createWriteStream(uploadPath));
-  return {
-    uploadStatus: "success",
-    filePath: path.join(__dirname, "../../", uploadPath),
-  };
+    await pump(file.file, fs.createWriteStream(uploadPath));
+    return {
+      status: "success",
+      fileName: `${uploadFileName}.${fileFromat}`,
+      filePath: path.join(__dirname, "../../", uploadPath),
+    };
+
 };
 
 export default upload;
