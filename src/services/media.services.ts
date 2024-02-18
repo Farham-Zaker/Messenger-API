70c5e334e36fad3366eb85d3cb5c4bb361b1a9c1
Farham-Zaker <farham.zaker007@gmail.com>
2024-02-18 11:15:47 +0330

    feat(media): Add findAll media service and its types

--- src/services/media.services.ts
+++ src/services/media.services.ts
@@ -2,13 +2,26 @@ import prismaServices from "../prisma/prismaServices";
 import {
   CreateMediaParamersTypes,
   CreatedMediaType,
+  FindAllMediaParametersTypes,
+  MediaType,
 } from "../types/mediaServices.types";
+import databaseSelector from "../utils/databaseSelector";
 
 class MediaServices {
   async createMany(data: CreateMediaParamersTypes[]): Promise<void> {
     await prismaServices.medias.createMany({
       data,
     });
+  }
+  async findAll({
+    condition,
+    selectedFields,
+  }: FindAllMediaParametersTypes): Promise<MediaType[]> {
+    const allMedia: MediaType[] = await prismaServices.medias.findMany({
+      where: condition,
+      select: databaseSelector("medias", selectedFields),
+    });
+    return allMedia;
   }
 }
 export default MediaServices;
--- src/types/mediaServices.types.ts
+++ src/types/mediaServices.types.ts
@@ -18,3 +18,64 @@ export type CreatedMediaType = {
   channelId?: string;
   groupId?: string;
 };
+export type FindAllMediaParametersTypes = {
+  condition: {
+    privateChatId?: string;
+    groupId?: string;
+    channelId?: string;
+  };
+  selectedFields: {
+    medias: string[];
+  };
+};
+export type MediaType = {
+  mediaId?: string;
+  filePath?: string;
+  fileType?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  privateChatId?: string;
+  privateChat?: PrivateChatTypes;
+  channelId?: string;
+  channel?: ChannelTypes;
+  groupId?: string;
+  group?: GroupTypes;
+  messageId?: string;
+  message?: MediaType | null;
+};
+type PrivateChatTypes = {
+  privateChatId: string;
+  user1Id: string;
+  user2Id: string;
+  updatedAt: Date;
+  createdAt: Date;
+};
+type ChannelTypes = {
+  channelId?: string;
+  title?: string;
+  bio?: string;
+  imagePath?: string | null;
+  ownerId?: string;
+  updatedAt?: Date;
+  createdAt?: Date;
+};
+type GroupTypes = {
+  groupId?: string;
+  title?: string;
+  bio?: string | null;
+  ownerId?: string;
+  imagePath?: string | null;
+  updatedAt?: Date;
+  createdAt?: Date;
+};
+type MessageTypes = {
+  messageId?: string;
+  text?: string;
+  senderId?: string;
+  createdAt?: Date;
+  replyOf?: string | null;
+  mediaId?: string | null;
+  privateChatId?: string | null;
+  groupId?: string | null;
+  channelId?: string | null;
+};
